e98bdc288a9a576e83d5178e133efa215b535583
cjremmett <cjremmett@gmail.com>
2025-02-18 19:11:24 -0500

    photo backend

--- photography.js
+++ photography.js
@@ -9,7 +9,7 @@ import * as path from 'path';
 
 import { MongoClient } from "mongodb";
 
-var ExifImage = require('exif').ExifImage;
+import { ExifImage } from 'exif';
 
 // Not port forwarded so creds can be in GitHub repo without issue
 const uri = "mongodb://admin:admin@192.168.0.121:27017";
